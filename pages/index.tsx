cd6f69975870b1c45b79b4c53f2b354d54c26346
Maxim Schmidt <maxim.schmidt@tum.de>
2023-02-05 13:47:17 +0100

    api

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,23 +6,30 @@ import InfoBox from '../components/InfoBox'
 import ShopArea from '../components/ShopArea'
 import WalletContextProvider from '../components/WalletContextProvider'
 
+import { MantineProvider } from '@mantine/core';
+import { ModalsProvider } from '@mantine/modals';
+
 export default function HomePage() {
   return (
-    <div className={styles.App}>
-      <Head>
-        <title>Get your SolBoard!</title>
-        <meta
-          name="description"
-          content="Get your SolBoard!"
-        />
-      </Head>
-      <WalletContextProvider>
-        <AppBar />
-        <div className={styles.AppBody}>
-            <InfoBox />
-            <ShopArea />
+    <MantineProvider>
+      <ModalsProvider>
+        <div className={styles.App}>
+          <Head>
+            <title>Get your SolBoard!</title>
+            <meta
+              name="description"
+              content="Get your SolBoard!"
+            />
+          </Head>
+          <WalletContextProvider>
+            <AppBar />
+            <div className={styles.AppBody}>
+                <InfoBox />
+                <ShopArea />
+            </div>
+          </WalletContextProvider >
         </div>
-      </WalletContextProvider >
-    </div>
+      </ModalsProvider>
+    </MantineProvider>
   );
 }
