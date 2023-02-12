2c60323314443491f159d059a3341c93a23e6c49
Maxim Schmidt <maxim.schmidt@tum.de>
2023-02-12 14:59:22 +0100

    new logo

--- components/AppBar.jsx
+++ components/AppBar.jsx
@@ -8,7 +8,7 @@ export default function AppBar({ onHeaderClick }) {
     return (
         <div className={styles.AppHeader}>
             <div className={styles.AppHeaderContainer}>
-                <img src="solboards.png" alt="SolBoards" onClick={onHeaderClick} style={{cursor:"pointer"}}/>
+                <Image src="logo.png" width={500} alt="SolBoards" onClick={onHeaderClick} style={{cursor:"pointer"}}/>
                 <WalletMultiButton />
             </div>
         </div>
