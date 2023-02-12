17cd7becaec77b7ca17a5bcb2e34725909ed78d4
Maxim Schmidt <maxim.schmidt@tum.de>
2023-02-12 13:15:39 +0100

    linkedin

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 import styles from '../styles/Home.module.css'
 
 import { Group, UnstyledButton, ActionIcon } from '@mantine/core';
-import { IconBrandTwitter, IconBrandGithub, IconMail } from '@tabler/icons';
+import { IconBrandTwitter, IconBrandGithub, IconMail, IconBrandLinkedin } from '@tabler/icons';
 
 export default function Footer() {
     return (
@@ -21,6 +21,11 @@ export default function Footer() {
                 <ActionIcon size="lg" radius="md" variant="outline" sx={{"&:hover": {"background-color": "#f5f5f5"}, "&:active": {transform: "none"}}}>
                   <a href="mailto:maxim.schmidt@tum.de">
                     <IconMail color="gray" size={24} />
+                  </a>
+                </ActionIcon>
+                <ActionIcon size="lg" color="dark" radius="md" variant="outline" sx={{"&:hover": {"background-color": "#f5f5f5"}, "&:active": {transform: "none"}}}>
+                  <a href="https://www.linkedin.com/in/maxim-schmidt" target="_blank">
+                    <IconBrandLinkedin color="rgb(37, 38, 43)" size={24} />
                   </a>
                 </ActionIcon>
             </Group>
